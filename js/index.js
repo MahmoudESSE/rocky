815fe03a5a9d3bf68ba5de80f73e9d1df6d99907
MahmoudESSE <mahmoudessehayli@gmail.com>
2025-02-09 12:08:35 +0100

    Add GUI interface

--- js/index.js
+++ js/index.js
@@ -1,40 +1,35 @@
+import { collapseTextChangeRangesAcrossMultipleVersions } from "typescript";
+
 /**
  * @constructor
  * @param {string} humanChoice
  * @param {string} computerChoice
+ * @returns {string}
  */
-function playRound(humanChoice, computerChoice) {
+function getPlayResult(humanChoice, computerChoice) {
   if (humanChoice == computerChoice) {
-    console.log("It's a tie!");
+    return 0;
   }
   if (humanChoice == "Rock" && computerChoice == "Paper") {
-    console.log("Computer Wins!");
+    return -1;
   }
   if (humanChoice == "Rock" && computerChoice == "Scissors") {
-    console.log("You Win!");
+    return 1;
   }
 
   if (humanChoice == "Paper" && computerChoice == "Scissors") {
-    console.log("Computer Wins!");
+    return -1;
   }
   if (humanChoice == "Paper" && computerChoice == "Rock") {
-    console.log("You Win!");
+    return 1;
   }
 
   if (humanChoice == "Scissors" && computerChoice == "Rock") {
-    console.log("Computer Wins!");
+    return -1;
   }
   if (humanChoice == "Scissors" && computerChoice == "Paper") {
-    console.log("You Wins!");
+    return 1;
   }
-}
-
-/**
- * @constructor
- * @returns {string}
- */
-function getHumanChoice() {
-  return prompt('Enter your Choise from "Rock", "Paper", "Scissors": ');
 }
 
 /**
@@ -54,7 +49,71 @@ function getComputerChoice() {
   }
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+/**
+ * @param {number} playerScore
+ * @param {number} computerScore
+ */
+function getGameScore(playerScore, computerScore) {
+  if (playerScore > computerScore) {
+    return 1;
+  }
+  if (playerScore < computerScore) {
+    return -1;
+  }
+  return 0;
+}
 
-playRound(humanSelection, computerSelection);
+function showGameScore(resultText) {
+  const body = document.querySelector("body");
+  console.log(`body: ${body.innerHTML}`);
+  const resultPara = document.createElement("p");
+  resultPara.innerHTML = resultText;
+  body.appendChild(resultPara);
+  console.log(`result: ${resultPara.innerHTML}`);
+  console.log(`body: ${body.innerHTML}`);
+}
+
+let humanSelection = "";
+
+const choiceBtns = document.querySelectorAll("button");
+let rounds = 0;
+let result = 0;
+let playerScore = 0;
+let computerScore = 0;
+
+choiceBtns.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (rounds === 5) {
+      const finalScore = getGameScore(playerScore, computerScore);
+      if (finalScore === 0) {
+        showGameScore("Result: It's a tie!");
+      }
+      if (finalScore === 1) {
+        showGameScore("You Win!");
+      }
+      if (finalScore === -1) {
+        showGameScore("Computer Wins!");
+      }
+    }
+
+    humanSelection = e.target.innerHTML;
+
+    const computerSelection = getComputerChoice();
+
+    result = getPlayResult(humanSelection, computerSelection);
+
+    if (result > 0) {
+      playerScore += result;
+    }
+    if (result < 0) {
+      computerScore += result * -1;
+    }
+
+    rounds += 1;
+    const playerScoreSpan = document.getElementById("player-score");
+    playerScoreSpan.innerHTML = playerScore;
+
+    const computercoreSpan = document.getElementById("computer-score");
+    computercoreSpan.innerHTML = computerScore;
+  });
+});
