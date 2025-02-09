8f8058498d5c1162acfc66bceb4dc75f70db24a2
MahmoudESSE <mahmoudessehayli@gmail.com>
2025-02-09 03:26:37 +0100

    style: add semi-colons to end of line

--- js/index.js
+++ js/index.js
@@ -1,44 +1,56 @@
+/**
+ * @constructor
+ * @param {string} humanChoice
+ * @param {string} computerChoice
+ */
 function playRound(humanChoice, computerChoice) {
   if (humanChoice == computerChoice) {
-    console.log("It's a tie!")
+    console.log("It's a tie!");
   }
   if (humanChoice == "Rock" && computerChoice == "Paper") {
-    console.log("Computer Wins!")
+    console.log("Computer Wins!");
   }
   if (humanChoice == "Rock" && computerChoice == "Scissors") {
-    console.log("You Win!")
+    console.log("You Win!");
   }
 
   if (humanChoice == "Paper" && computerChoice == "Scissors") {
-    console.log("Computer Wins!")
+    console.log("Computer Wins!");
   }
   if (humanChoice == "Paper" && computerChoice == "Rock") {
-    console.log("You Win!")
+    console.log("You Win!");
   }
 
   if (humanChoice == "Scissors" && computerChoice == "Rock") {
-    console.log("Computer Wins!")
+    console.log("Computer Wins!");
   }
   if (humanChoice == "Scissors" && computerChoice == "Paper") {
-    console.log("You Wins!")
+    console.log("You Wins!");
   }
 }
 
+/**
+ * @constructor
+ * @returns {string}
+ */
 function getHumanChoice() {
-  return prompt('Enter your Choise from "Rock", "Paper", "Scissors": ')
+  return prompt('Enter your Choise from "Rock", "Paper", "Scissors": ');
 }
 
-
+/**
+ * @constructor
+ * @returns {string}
+ */
 function getComputerChoice() {
-  let chance = Math.random() * 3
+  let chance = Math.random() * 3;
   if (chance >= 2) {
-    return "Rock"
+    return "Rock";
   }
   if (chance >= 1) {
-    return "Scissors"
+    return "Scissors";
   }
   if (chance >= 0) {
-    return "Paper"
+    return "Paper";
   }
 }
 
